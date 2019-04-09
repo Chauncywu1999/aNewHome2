cb8e6e5e7c4f8563c8a288bd4bb920f5370036bd
Chauncywu1999 <Chauncywu1999@163.com>
2019-04-09 17:42:26 +0800

    the first submit the whole project code

---
+++ Outside.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aHome
+{
+    class Outside:Location
+    {
+        private bool hot;
+
+        public Outside (string name,bool hot):base(name)
+        {
+            this.hot = hot;
+        }
+
+        public override string Description
+        {
+            get
+            {
+                string newDescription = base.Description;
+                if (hot)
+                    newDescription += "   It's very hot.";
+                return newDescription;
+            }
+        }
+    }
+
+    class OutsideWithDoor:Outside,IHasExteriorDoor
+    {
+        public OutsideWithDoor(string name,bool hot,string doorDescription):base(name,hot)
+        {
+            this.DoorDescription = doorDescription;
+        }
+
+        public string DoorDescription { get; private set; }
+        public Location DoorLocation { get; set; }
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " You see " + DoorDescription + ".";
+            }
+        }
+    }
+
+    class OutsideWithHidingPlace:Outside,IHidingPlace
+    {
+        public OutsideWithHidingPlace(string name,bool hot,string hidingPlaceName):base(name,hot)
+        {
+            HidingPlaceName = hidingPlaceName;
+        }
+
+        public string HidingPlaceName { get; private set; }
+
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Someone could hide " + HidingPlaceName + ".";
+            }
+        }
+    }
+}
---
+++ Room.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aHome
+{
+    class Room:Location
+    {
+        private string decoration;
+
+        public Room(string name,string decoration):base(name)
+        {
+            this.decoration = decoration;
+        }
+
+        public override string Description
+        {
+            get { return base.Description + " You see " + decoration + "."; }
+        }
+    }
+
+    class RoomWithDoor:RoomWithHidingPlace,IHasExteriorDoor
+    {
+        public RoomWithDoor(string name,string decoration, string hidingPlaceName , string doorDescription):base(name,decoration,hidingPlaceName)
+        {
+            DoorDescription = doorDescription;
+        }
+
+        public string DoorDescription { get;private set; }
+
+        public Location DoorLocation { get; set; }
+    }
+
+    class RoomWithHidingPlace:Room,IHidingPlace
+    {
+        public RoomWithHidingPlace(string name,string decoration,string hidingPlaceName):base(name,decoration)
+        {
+            HidingPlaceName = hidingPlaceName;
+        }
+
+        public string HidingPlaceName { get; private set; }
+        public override string Description
+        {
+            get
+            {
+                return base.Description + " Someone could hide " + HidingPlaceName + ".";
+            }
+        }
+    }
+}
